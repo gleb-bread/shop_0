211c7b51af603885fed41e5b6661ae8a0e0c4d6c
Иванов Глеб <glebhets123@gmail.com>
2023-05-01 21:20:25 +0300

    add show product on cart

--- cart.php
+++ cart.php
@@ -1,7 +1,12 @@
 <?php
-	include "./server/connectionDB.php";
-	include "./header.php";
-	include "./nav-menu.php";
+	include './server/connectionDB.php';
+	include './header.php';
+	include './nav-menu.php';
+    $idSession = $_COOKIE['id__session'];
+    $getInfoCart = mysqli_query($resultConntection, "SELECT * FROM `shop__cart` WHERE 
+    `id__session`='$idSession'");
+    $countItems = 0;
+    $countPrice = 0;
 ?>
 <section class="cart">
 <div class="cart__wrapper">
@@ -17,27 +22,59 @@
                 <div class="cart__item__all__price">Сумма</div>
             </div>
         </div>
+        <?php while($infoCartItem = mysqli_fetch_assoc($getInfoCart)){?>
         <div class="cart__item">
             <div class="cart__item__block__1">
                 <div class="cart__item__icon">
-                    <img src="./style/img/product/sofa/sofa5.jpg" alt="">
+                    <img src="<?php
+                        $idProduct =  $infoCartItem['id__product'];
+                        $queryImgProduct =  mysqli_query($resultConntection, "SELECT `img__product` FROM `shop__product` 
+                        WHERE `id__product`='$idProduct'");
+                        $imgProduct = mysqli_fetch_array($queryImgProduct);
+                        echo $imgProduct['img__product'];
+                    ?>" alt="">
                 </div>
-                <div class="cart__item__name">Кресло креслое</div>
-                <div class="cart__item__price">2000</div>
+                <div class="cart__item__name"><?php
+                        $idProduct =  $infoCartItem['id__product'];
+                        $queryNameProduct = mysqli_query($resultConntection, "SELECT `name__product` FROM `shop__product` 
+                        WHERE `id__product`='$idProduct'");
+                        $nameProduct = mysqli_fetch_array($queryNameProduct);
+                        echo $nameProduct['name__product'];
+                    ?></div>
+                <div class="cart__item__price"><?php
+                        $idProduct =  $infoCartItem['id__product'];
+                        $queryPriceProduct =  mysqli_query($resultConntection, "SELECT `price__product` FROM `shop__product` 
+                        WHERE `id__product`='$idProduct'");
+                        $priceProduct = mysqli_fetch_array($queryPriceProduct);
+                        echo $priceProduct['price__product'];
+                    ?>&#8381;</div>
             </div>
             <div class="cart__item__block__2">
-                <div class="cart__item__count">5</div>
-                <div class="cart__item__all__price">10000</div>
+                <div class="cart__item__count"><?php echo $infoCartItem['count__product']?></div>
+                <div class="cart__item__all__price"><?php echo (
+                    $infoCartItem['count__product']  * $priceProduct['price__product']);
+                    $countPrice += $infoCartItem['count__product']  * $priceProduct['price__product'];
+                    ?></div>
             </div>
         </div>
-        
+        <?php 
+            $countItems += 1;
+        }
+            if ($countItems != 0) {
+        ?>
+
         <div class="cart__final-price">
             <p>Итого:</p>
-            <p>100000&#8381;</p>
+            <p><?php echo $countPrice;?>&#8381;</p>
         </div>
         <div class="cart__btn-buy__wrapper">
             <div class="cart__btn-buy">Оплатить</div> 
         </div>
+        <?php }
+         else {
+        ?>
+            В корзине пока ничего нет.
+        <?php }?>
     </div>
 </section>
 <?php
